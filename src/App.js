07ff5d27d0bc38d7c8f9f4416243ef7e2137c427
Kristian <kristian@coulsy.co.uk>
2020-07-10 13:02:14 +0100

    FE: added basic routing with history

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+import history from './helpers/history';
+
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
     return (
-        <div className="App">
-            <h1>hello world</h1>
-        </div>
+        <Router history={history}>
+            <div className="App">
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+                <h1>hello world</h1>
+                <Switch>
+                    <Route exact path="/"><Home /></Route>
+                    <Route path="/register"><Register /></Route>
+                    <Route path="/login"><Login /></Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
