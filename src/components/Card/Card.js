b7a311f9f2dfeee82d83ec628231f180304cc231
Kristian <kristian@coulsy.co.uk>
2020-07-10 14:08:41 +0100

    FE: added card component with title, centered, fluid props

---
+++ src/components/Card/Card.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Card({title, children, centered, fluid}) {
+    let classNames = 'ui card';
+
+    if (centered) classNames += ' centered';
+    if (fluid) classNames += ' fluid';
+
+    return (
+        <div className={classNames}>
+            {title ? (<div className="content">
+                <div className="header">{title}</div>
+            </div>) : null}
+            <div className="content">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
