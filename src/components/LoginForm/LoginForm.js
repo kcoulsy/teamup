6b8f6bf5d88d3a99ee080b32d4f675f61926a291
Kristian <kristian@coulsy.co.uk>
2020-07-10 14:29:44 +0100

    FE: Added basic form validation to login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Card from './../Card/Card';
 
 function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const defaultErrorObj = { usernameEmpty: false, passwordEmpty: false };
+    const [error, setError] = useState(defaultErrorObj);
+
+    const submitForm = (ev) => {
+        ev.preventDefault();
+        if (!username.length || !password.length) {
+            setError({
+                usernameEmpty: !username.length,
+                passwordEmpty: !password.length
+            });
+        } else {
+            setError(defaultErrorObj);
+        }
+    }
+
+    const formError = Object.values(error).includes(true);
     return (
         <Card title="Login" centered>
-            <form className="ui form">
-                <div className="field">
+            <form className={`ui form${formError ? ' error' : ''}`}>
+                <div className={`field${error.usernameEmpty ? ' error' : ''}`}>
                     <label>Username</label>
-                    <input type="text" name="username" placeholder="Username" />
+                    <input type="text" name="username" placeholder="Username" onChange={ev => setUsername(ev.target.value)}/>
                 </div>
-                <div className="field">
+                <div className={`field${error.passwordEmpty ? ' error' : ''}`}>
                     <label>Password</label>
-                    <input type="password" name="password" placeholder="Password" />
+                    <input type="password" name="password" placeholder="Password" onChange={ev => setPassword(ev.target.value)}/>
                 </div>
-                <button className="ui button" type="submit">Login</button>
+                <button className="ui button" type="submit" onClick={submitForm}>Login</button>
                 Not registered? <Link to="/register">Register</Link>
+                {Object.values(error).includes(true) ? (
+                    <div className="ui error message">
+                        <ul className="list">
+                            {error.usernameEmpty ? <li>Please enter a username</li> : null}
+                            {error.passwordEmpty ? <li>Please enter a password</li> : null}
+                        </ul>
+                    </div>
+                ): null}
             </form>
         </Card>
     );
