e9e855a70275af6e981c2cbb34d7f2867d10dee6
Kristian <kristian@coulsy.co.uk>
2020-07-10 11:12:33 +0100

    BE: initial express server and mongoose connection

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connection established');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
