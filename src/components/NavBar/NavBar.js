4133d52ca0bfd17cdf11dd452e75a93a811b377d
Kristian <kristian@coulsy.co.uk>
2020-07-20 16:33:34 +0100

    Added basic styling to navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -6,31 +6,38 @@ import { startLogout } from './../../actions/auth';
 import isLoggedIn from '../../helpers/isLoggedIn';
 
 function NavBar({ isLoggedIn, startLogout }) {
+    const handleLogout = (ev) => {
+        ev.preventDefault();
+        startLogout();
+    };
+
     return (
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            {!isLoggedIn ? (
-                <React.Fragment>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                </React.Fragment>
-            ) : (
-                <React.Fragment>
-                    <li>
-                        <button onClick={startLogout}>Logout</button>
-                    </li>
-                    <li>
-                        <Link to="/secret">Secret</Link>
-                    </li>
-                </React.Fragment>
-            )}
-        </ul>
+        <div className="ui menu">
+            <Link to="/" className="header item">
+                Home
+            </Link>
+            <div className="right menu">
+                {!isLoggedIn ? (
+                    <Fragment>
+                        <Link to="/register" className="item">
+                            Register
+                        </Link>
+                        <Link to="/login" className="item">
+                            Login
+                        </Link>
+                    </Fragment>
+                ) : (
+                    <Fragment>
+                        <Link to="/secret" className="item">
+                            Secret
+                        </Link>
+                        <a href="/" className="item" onClick={handleLogout}>
+                            Logout
+                        </a>
+                    </Fragment>
+                )}
+            </div>
+        </div>
     );
 }
 const mapStateToProps = (state) => {
