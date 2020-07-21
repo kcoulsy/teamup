95c3ccdbc67c64d4fe67f0761c2f91aa8eb5e508
Kristian <kristian@coulsy.co.uk>
2020-07-21 14:46:30 +0100

    Added modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Portal from './../Portal';
+
+/**
+ * Modal
+ *
+ * @param {actionsObject[]} actions - an array of objects containing button details
+ *
+ * @interface actionsObject
+ * @type {Object}
+ * @property {string} buttonLabel -label for the buttons
+ * @property {function} onClick - onClick event handlers
+ * @property {string} klasses - single string appended to existing classNames
+ */
+function Modal({ isActive = false, children, headerTitle, actions = [] }) {
+    return (
+        <Portal>
+            <div
+                className={`ui dimmer modals page visible ${isActive ? ' active' : ''}`}
+                style={{ display: 'flex !important' }}>
+                <div className="ui standard test modal visible active front" style={{ display: 'flex !important' }}>
+                    {headerTitle ? <div className="header">{headerTitle}</div> : null}
+                    <div className="content">{children}</div>
+                    <div className="actions"></div>
+                    {actions.length ? (
+                        <div className="actions">
+                            {actions.map((actionObj, i) => {
+                                let klasses = 'ui button';
+                                const key = actionObj.buttonLabel + '_' + i;
+
+                                if (actionObj.klasses) klasses += ` ${actionObj.klasses}`;
+
+                                return (
+                                    <div key={key} className={klasses} onClick={actionObj.onClick}>
+                                        {actionObj.buttonLabel}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    ) : null}
+                </div>
+            </div>
+        </Portal>
+    );
+}
+
+export default Modal;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '../components/Modal/Modal';
 
 function Home() {
+    const [modalActive, setModalActive] = useState(true);
+    const toggleModal = (ev) => {
+        ev.preventDefault();
+        setModalActive(!modalActive);
+    };
     return (
         <div>
             <h3>Home Page</h3>
+            <button className="ui button" onClick={toggleModal}>
+                Open Modal
+            </button>
+            <Modal
+                headerTitle="hello"
+                isActive={modalActive}
+                actions={[{ buttonLabel: 'Close', klasses: 'positive right labeled icon ', onClick: toggleModal }]}>
+                this is content
+            </Modal>
         </div>
     );
 }
