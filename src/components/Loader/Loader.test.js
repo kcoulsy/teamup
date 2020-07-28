9aa1161388ceb6812674a886d311edcebca64120
Kristian <kristian@coulsy.co.uk>
2020-07-28 11:01:56 +0100

    test: added tests for loader component

---
+++ src/components/Loader/Loader.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Loader from './Loader';
+import { shallowToJson } from 'enzyme-to-json';
+
+describe('render Loader component', () => {
+    it('should render correctly', () => {
+        const wrapper = shallow(<Loader />);
+
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render as active when isLoading is true', () => {
+        const wrapper = shallow(<Loader isLoading />);
+
+        expect(wrapper.find('.active').length).toBe(1);
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+
+        wrapper.setProps({ isLoading: false });
+        expect(wrapper.find('.active').length).toBe(0);
+    });
+
+    it('should render as inverted when dark is false (inverted = light theme)', () => {
+        const wrapper = shallow(<Loader />);
+
+        expect(wrapper.find('.inverted').length).toBe(1);
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+
+        wrapper.setProps({ dark: true });
+        expect(wrapper.find('.inverted').length).toBe(0);
+    });
+
+    it('should render children correctly', () => {
+        const wrapper = shallow(
+            <Loader>
+                <p>JSX Child</p>
+            </Loader>
+        );
+
+        expect(wrapper.contains(<p>JSX Child</p>)).toBe(true);
+        expect(shallowToJson(wrapper)).toMatchSnapshot();
+    });
+});
