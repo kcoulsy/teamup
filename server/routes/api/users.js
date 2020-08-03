42916118ff86a0ddf84ab83b4d3ff8f672f66ecb
Kristian <kristian@coulsy.co.uk>
2020-08-03 11:23:35 +0100

    chore: commented out unused route but left in as an example

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-let User = require('../../models/user.model');
+// Unused but left in as an example of using the Auth middleware
 
-const { Authenticate } = require('../../middleware/authenticate');
+// let User = require('../../models/user.model');
 
-router.route('/').get(Authenticate, (req, res) => {
-    User.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(400).json('Error: ' + err));
-});
+// const { Authenticate } = require('../../middleware/authenticate');
+
+// router.route('/').get(Authenticate, (req, res) => {
+//     User.find()
+//         .then((users) => res.json(users))
+//         .catch((err) => res.status(400).json('Error: ' + err));
+// });
 
 module.exports = router;
