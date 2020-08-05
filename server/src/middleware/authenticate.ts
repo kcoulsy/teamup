2d20871418d881da050fd9ebb134373e8f3c90c9
Kristian <kristian@coulsy.co.uk>
2020-08-05 11:25:48 +0100

    lint: fixed extra space

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -1,7 +1,7 @@
 import User from '../models/user.model';
 import * as express from 'express';
 import { RES_AUTH_HEADER } from '../constants/auth'
-import { NextFunction} from 'express';
+import { NextFunction } from 'express';
 
 const Authenticate = (req : express.Request, res : express.Response, next : NextFunction) => {
     const token = req.header(RES_AUTH_HEADER);
