4936a267491f631f1932f88fc8690a000cbf355d
Kristian <kristian@coulsy.co.uk>
2020-09-28 17:03:45 +0100

    build: 0.1

---
+++ build/precache-manifest.e551d888c4caac9719f2735bcb158775.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a4fcf0c8d1a5590a5d902282f50fd68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7525f7dd11bfd7d266d6",
+    "url": "/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "21f33ba7e83e6e5565b0",
+    "url": "/static/css/main.566f8300.chunk.css"
+  },
+  {
+    "revision": "7525f7dd11bfd7d266d6",
+    "url": "/static/js/2.24690c8c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.24690c8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21f33ba7e83e6e5565b0",
+    "url": "/static/js/main.d7cc6b48.chunk.js"
+  },
+  {
+    "revision": "8cc77969fff57efcb6db",
+    "url": "/static/js/runtime-main.6491e447.js"
+  }
+]);
